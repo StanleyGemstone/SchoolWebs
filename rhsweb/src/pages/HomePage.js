c0d0ecbd866bc952363eb83bf74c1476094019cd
StanleyGemstone <stanleynduchi@gmail.com>
2024-12-23 12:46:17 +0100

    adjusted hompage though no logo yet

--- rhsweb/src/pages/HomePage.js
+++ rhsweb/src/pages/HomePage.js
@@ -1,36 +1,3 @@
-// import React from "react";
-// import { Helmet } from "react-helmet";
-// import "../styles/HomePage.scss";
-
-// const HomePage = () => {
-//   return (
-//     <div className="homepage">
-//       <Helmet>
-//         <title>RHS-Home</title>
-//       </Helmet>
-//       <header className="homepage__header">
-//         <div className="homepage__logo">
-//           <h1>Royal Heritage Schools</h1>
-//         </div>
-//         <nav className="homepage__nav">
-//           <ul>
-//             <li><a href="/home">Home</a></li>
-//             <li><a href="/about">About</a></li>
-//             <li><a href="/signin">Signin</a></li>
-//             <li><a href="/signup">Signup</a></li>
-//           </ul>
-//         </nav>
-//       </header>
-//       <main className="homepage__main">
-//         <h2>Welcome to Our School</h2>
-//         <p>Empowering students for a brighter future!</p>
-//       </main>
-//     </div>
-//   );
-// };
-
-// export default HomePage;
-
 import React from "react";
 import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
@@ -54,7 +21,7 @@ const Homepage = () => {
       </Helmet>
       <header className="homepage-header">
         <div className="logo">
-          <img src="../utils/logo.png" alt="School Logo" />
+          <img src="../utils/logo.PNG" alt="School Logo" />
           <h1>ROYAL HERITAGE SCHOOL</h1>
         </div>
         <nav className="homepage-nav">
@@ -67,7 +34,7 @@ const Homepage = () => {
             </li>
             {!isLoggedIn ? (
               <li>
-                <Link to="/login">Login</Link>
+                <Link to="/signin">Login</Link>
               </li>
             ) : (
               <li>
@@ -90,7 +57,7 @@ const Homepage = () => {
           students, and administrators can access their portals to streamline
           academic processes.
         </p>
-        <Link to="/login" className="cta-btn">
+        <Link to="/signup" className="cta-btn">
           Get Started
         </Link>
       </main>
